80881ee59a728fa5e4f654bfdf8ce67d9b6a29ab
herrenchek <giselle.herr@icloud.com>
2021-06-16 23:45:18 -0400

    Very basic component

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,11 @@ import React from 'react';
 class App extends React.Component {
     render() {
         return (
-            <div></div>
+            <div>
+                <h1>Book Title</h1>
+                <h2>Author</h2>
+                <p>Description</p>
+            </div>
         );
     }
 }
